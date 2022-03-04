d076558ab9fe7148621cb5243d73d5bff4d5c47e
Park Ji Gun <bjigeon@gmail.com>
2022-03-04 16:40:49 +0900

    업데이트

--- React_class/blog/src/App.js
+++ React_class/blog/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집'
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <h4>{posts}</h4>
     </div>
   );
 }

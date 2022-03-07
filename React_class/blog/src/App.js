445e8c087c0b0dc378d94afb201105fccf57b278
Park Ji Gun <bjigeon@gmail.com>
2022-03-07 21:00:26 +0900

    수정 & 작성

--- React_class/blog/src/App.js
+++ React_class/blog/src/App.js
@@ -1,18 +1,26 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [a,b] = useState(['남자 코트 추천', '강남 우동 맛집']);
+
   let posts = '강남 고기 맛집'
+  let css = { color : 'yellow',fontSize : '30px' }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className='list'>
+        <h3>{a[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 

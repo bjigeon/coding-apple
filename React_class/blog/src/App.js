6c63135232bd366f56c5364f45388b20439222ee
Park Ji Gun <bjigeon@gmail.com>
2022-03-08 19:22:28 +0900

    숙제하기 & 수정 & 추가

--- React_class/blog/src/App.js
+++ React_class/blog/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 
 function App() {
 
-  let [a,b] = useState(['남자 코트 추천', '강남 우동 맛집']);
-
+  let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬독학']);
   let posts = '강남 고기 맛집'
   let css = { color : 'yellow',fontSize : '30px' }
 
@@ -14,9 +13,16 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+
       <div className='list'>
-        <h3>{a[1]}</h3>
+        <h3>{글제목[0]}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+        <h3>{글제목[1]}</h3>
+        <p>2월 18일</p>
+        <hr/>
+        <h3>{글제목[2]}</h3>
+        <p>2월 19일</p>
         <hr/>
       </div>
     </div>

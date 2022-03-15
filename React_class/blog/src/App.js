fe940dcbda6f925c358e9193ca5ff5fd4ea1b4a1
bjigeon <79364171+bjigeon@users.noreply.github.com>
2022-03-15 18:21:12 +0900

    삭제 & 수정

--- React_class/blog/src/App.js
+++ React_class/blog/src/App.js
@@ -46,22 +46,19 @@ function App() {
         <hr />
       </div>
 
-      <Modal/>
-      
+      <Modal />
     </div>
   );
 }
 
-function Modal(){
+function Modal() {
   return (
     <div className="modal">
       <h2>제목</h2>
       <p>날짜</p>
       <p>상세내용</p>
-    </div> 
-  )
+    </div>
+  );
 }
-
- 
 
 export default App;

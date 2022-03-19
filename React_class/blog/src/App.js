4fb62040878c86556feaacf153e8463183385563
bjigeon <79364171+bjigeon@users.noreply.github.com>
2022-03-19 18:29:47 +0900

    수정 & 추가

--- React_class/blog/src/App.js
+++ React_class/blog/src/App.js
@@ -3,12 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
-  let posts = "강남 고기 맛집";
-  let [따봉, 따봉변경] = useState(0);
   let css = { color: "yellow", fontSize: "30px" };
-
-  let [modal,modal변경] = useState(false);
+  let posts = "강남 고기 맛집";
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal,modal변경] = useState(false);  
+  let [누른제목, 누른제목변경] = useState(0);
 
   function 제목바꾸기() {
     var newArray = [...글제목];
@@ -22,17 +22,10 @@ function App() {
         <div>개발 Blog</div>
       </div>
 
-      <div className="list">
+      {/* <div className="list">
         <h3>
           {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>
-          {따봉}
+          <span onClick={() => { 따봉변경(따봉 + 1); }}>👍</span>{따봉}
         </h3>
 
         <p>2월 17일 발행</p>
@@ -45,11 +38,11 @@ function App() {
         <hr />
       </div>
       <div className="list">
-        {/* <h3 onClick={() => {modal변경(modal = true)}}>{글제목[2]}</h3> */}
+        <h3 onClick={() => {modal변경(modal = true)}}>{글제목[2]}</h3>
         <h3 onClick={() => {modal변경(true)}}>{글제목[2]}</h3>
         <p>2월 19일</p>
         <hr />
-      </div>
+      </div> */}
       {/* {modal == true ? <Modal /> : null} */}
 
 
@@ -61,23 +54,46 @@ function App() {
         : null
       } */}
 
+      
+
+      {/* 글제목의 갯수 만큼 */}
+      {/* 글 : 글제목안에 있는 하나하나의 데이터 값 */}
+      {/* i : 카운트 수 */}
+      {
+        글제목.map(function(글,i){
+          return ( 
+            <div className="list">
+            <h3 onClick={() => {누른제목변경(i)}}> {글} 
+              <span onClick={() => { 따봉변경(따봉 + 1); }}>👍</span>
+              {따봉}
+            </h3>
+            <p>2월 18일</p>
+            <hr />
+          </div>
+          )
+        })
+      }
+
+      {/* <button onClick={() => {누른제목변경(0)}}>버튼1</button>
+      <button onClick={() => {누른제목변경(1)}}>버튼2</button>
+      <button onClick={() => {누른제목변경(2)}}>버튼3</button> */}
 
       {/* 애플코딩 */}
-      {/* <button onClick={ ()=>{ modal변경(!modal) } }> 열고닫는버튼 </button>
+      <button onClick={ ()=>{ modal변경(!modal) } }> 열고닫는버튼 </button>
       { 
         modal == true 
-        ? <Modal />
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
-      } */}
-      
+      }
+
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
